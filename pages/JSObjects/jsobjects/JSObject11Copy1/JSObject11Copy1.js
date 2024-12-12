644ac8a1245a95592dd4b5b9e3cc279467417caa
Mohammad Shadab Buchh <shadab@appsmith.com>
2024-12-12 08:47:38 +0000

    commit

---
+++ pages/JSObjects/jsobjects/JSObject11Copy1/JSObject11Copy1.js
@@ -0,0 +1,20 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	blahBlahBlah () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		setInterval(() => {
+			showAlert("Hi, this is JS Objects!", "warning")
+		}, 2500, "Interval")
+	},
+	this () {
+	},
+	kjshf () {
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+		clearInterval("Interval")
+	}
+}
